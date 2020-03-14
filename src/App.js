10343844f21002bec202797795a4983642633f2a
Gustavo Diniz Monteiro <dinizmonteirogustavo@gmail.com>
2020-03-14 14:38:54 -0300

    Add basic style libraries and create router module.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+import Feed from './screens/Feed'
 
 function App() {
-  return <div/>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Feed}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
